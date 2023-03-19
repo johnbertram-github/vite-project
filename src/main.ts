55e721f32d2cc655b5a96b556ef9b983d2498cc8
John Bertram <johnbertram.uk@hotmail.com>
2023-03-19 18:03:58 +0000

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import 'primeflex/primeflex.css'
+// import 'primeflex/primeflex.css'
 
-import './style.css'
+// import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
